4a596bf4fd8f89d008ff36e62ac492af271dcd34
Mohamed <mohasalanka2020@gmail.com>
2023-04-20 10:25:04 +0300

    Done

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 // use these variables in your JSX (update them with your own name and city!)
 
 const name = "Moha";
-const city = "Nairobi";
+const city = "Mogadishu";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
